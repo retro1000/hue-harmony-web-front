f99827e0515c8ce3158571f56c38ea4e924704b0
retro1000 <damithajayawardana678@gmail.com>
2024-07-24 01:20:00 +0530

    "Updated PopupFormDialog, NumberFormatField, and ProductList components with new fields, props, and functionality."

--- src/app/components/InputField/NumberFormatField.jsx
+++ src/app/components/InputField/NumberFormatField.jsx
@@ -2,7 +2,7 @@ import react from 'react'
 import { NumericFormat } from 'react-number-format';
 import { TextField } from '@mui/material';
 
-function NumberFormatField({ textFieldProps, allowNegative, decimalScale, fixedDecimalScale, ...rest }){
+function NumberFormatField({ textFieldProps, allowNegative, decimalScale, fixedDecimalScale, min, max, ...rest }){
 
     return (
         <NumericFormat
@@ -12,6 +12,13 @@ function NumberFormatField({ textFieldProps, allowNegative, decimalScale, fixedD
           allowNegative={allowNegative}
           inputProps={textFieldProps}
           {...rest}
+          isAllowed={(values) => {
+            if(min && max){
+              const { floatValue } = values;
+              return floatValue >= min && floatValue <= max;
+            }
+            return true;
+          }}
         />
       );
 }
