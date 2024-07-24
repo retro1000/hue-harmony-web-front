1e298c0d6a5ef7f4e6c5d46ff3fce8e5467e5e81
iamluwin <luwin.anuka@gmail.com>
2024-07-24 23:27:17 +0530

    Order Summary component and Full view

---
+++ src/app/components/BillingDetails/OrderSummary.jsx
@@ -0,0 +1,87 @@
+
+import React from "react";
+import { Typography, Grid, Divider, Button, TextField ,  FormControlLabel,
+  Radio,
+  RadioGroup,} from "@mui/material";
+
+const OrderSummary = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h6" gutterBottom>
+          Order Summary
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography>LCD Monitor</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography align="right">$650</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography>H1 Gamepad</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography align="right">$1100</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography>Subtotal:</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography align="right">$1750</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography>Shipping:</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography align="right">Free</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h6">Total:</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h6" align="right">
+          $1750
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField fullWidth label="Coupon Code" variant="outlined" />
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" fullWidth>
+          Apply Coupon
+        </Button>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="h6" gutterBottom>
+          Payment Method
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <RadioGroup defaultValue="bank" name="payment-method">
+          <FormControlLabel value="bank" control={<Radio />} label="Bank" />
+          <FormControlLabel
+            value="cash"
+            control={<Radio />}
+            label="Cash on delivery"
+          />
+        </RadioGroup>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button variant="contained" color="secondary" fullWidth>
+          Place Order
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default OrderSummary;
