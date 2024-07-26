bcd32ac91dc2b5624f71f9152d545906113fc121
iamluwin <luwin.anuka@gmail.com>
2024-07-26 23:43:15 +0530

    ContactUs Page and route

--- src/app/views/home/home-routes.js
+++ src/app/views/home/home-routes.js
@@ -3,13 +3,25 @@ import Loadable from "app/components/Loadable";
 
 
 const Home = Loadable(lazy(() => import('../home/MainLayout.jsx')))
+const AboutPage = Loadable(lazy(() => import('../home/AboutPage')))
+const ContactPage = Loadable(lazy(() => import('../home/ContactPage.jsx')))
+
 
 
 const homeRoutes = [
   {
     path: "/Home",
     element: <Home />
-  }
+  },
+  {
+    path: "/About",
+    element: <AboutPage />
+  },
+  {
+    path: "/Contactus",
+    element: <ContactPage />
+  },
+  
 ];
 
 export default homeRoutes;
