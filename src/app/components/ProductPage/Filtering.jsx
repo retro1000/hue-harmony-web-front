6843d60279fc02a84fbff3e5c77f6355bf074342
iamluwin <luwin.anuka@gmail.com>
2024-07-27 22:58:46 +0530

    add Accordion to filter product page

--- src/app/components/ProductPage/Filtering.jsx
+++ src/app/components/ProductPage/Filtering.jsx
@@ -1,86 +1,73 @@
-
-import { Card, CardContent, Typography, Checkbox, Button, FormControlLabel, FormGroup, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+// FilterBar.jsx
+import React, { useState } from 'react';
+import { Box, Typography, FormGroup, FormControlLabel, Checkbox, Button, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+export const filtersConfig = [
+  {
+    category: 'Position',
+    options: ['Exterior', 'Interior', 'Interior and Exterior'],
+  },
+  {
+    category: 'Product type',
+    options: ['Cleaner', 'Paint', 'Undercoat', 'Varnish', 'Waterproofing'],
+  },
+  {
+    category: 'Room types',
+    options: [
+      'Bathroom',
+      'Bedroom',
+      "Children's Room",
+      'Dining Room',
+      'Hallway',
+      'Home Office',
+      'Kitchen',
+      'Living Room',
+    ],
+  },
+  {
+    category: 'Surface',
+    options: ['Bluestone', 'Doors', 'Furniture', 'Metal', 'Walls', 'Windows', 'Wood'],
+  },
+  {
+    category: 'Finish',
+    options: ['Gloss', 'Gloss, Semi Gloss Matt', 'High Gloss', 'Low Sheen', 'Matt', 'Mid Sheen', 'NA', 'Semi Gloss'],
+  },
+];
 
-export const Filters = ({ filters, handleFilterChange, handleClearAll }) => (
-  <Card style={{ marginBottom: 16 }}>
-    <CardContent>
-      <Typography variant="h6">Filters</Typography>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography variant="subtitle1">Position</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox checked={filters.positionExterior} onChange={handleFilterChange} name="positionExterior" />}
-              label="Exterior"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.positionInterior} onChange={handleFilterChange} name="positionInterior" />}
-              label="Interior"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.positionInteriorExterior} onChange={handleFilterChange} name="positionInteriorExterior" />}
-              label="Interior and Exterior"
-            />
-          </FormGroup>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography variant="subtitle1">Product type</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox checked={filters.productTypeCleaner} onChange={handleFilterChange} name="productTypeCleaner" />}
-              label="Cleaner"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.productTypePaint} onChange={handleFilterChange} name="productTypePaint" />}
-              label="Paint"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.productTypeUndercoat} onChange={handleFilterChange} name="productTypeUndercoat" />}
-              label="Undercoat"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.productTypeVarnish} onChange={handleFilterChange} name="productTypeVarnish" />}
-              label="Varnish"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.productTypeWaterproofing} onChange={handleFilterChange} name="productTypeWaterproofing" />}
-              label="Waterproofing"
-            />
-          </FormGroup>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography variant="subtitle1">Room types</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox checked={filters.roomTypeBathroom} onChange={handleFilterChange} name="roomTypeBathroom" />}
-              label="Bathroom"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={filters.roomTypeBedroom} onChange={handleFilterChange} name="roomTypeBedroom" />}
-              label="Bedroom"
-            />
-            {/* Add more room types as needed */}
-          </FormGroup>
-        </AccordionDetails>
-      </Accordion>
-      <br />
-      <Button variant="contained" color="primary" onClick={handleClearAll}>
-        Clear All
-      </Button>
-    </CardContent>
-  </Card>
+const FilterList = ({ filter, filters, handleFilterChange }) => (
+  <Accordion>
+    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography variant="h6">{filter.category}</Typography>
+    </AccordionSummary>
+    <AccordionDetails>
+      <FormGroup>
+        {filter.options.map((option, index) => (
+          <FormControlLabel
+            key={index}
+            control={
+              <Checkbox
+                checked={filters[`${filter.category.toLowerCase().replace(/ /g, '')}${option.replace(/ /g, '')}`] || false}
+                onChange={handleFilterChange}
+                name={`${filter.category.toLowerCase().replace(/ /g, '')}${option.replace(/ /g, '')}`}
+              />
+            }
+            label={option}
+          />
+        ))}
+      </FormGroup>
+    </AccordionDetails>
+  </Accordion>
 );
 
+const FilterBar = ({ filters, handleFilterChange, handleClearAll }) => {
+  return (
+    <>
+      {filtersConfig.map((filter, index) => (
+        <FilterList key={index} filter={filter} filters={filters} handleFilterChange={handleFilterChange} />
+      ))}
+    </>
+  );
+};
 
+export default FilterBar;
--- src/app/views/product/FilterProduct.jsx
+++ src/app/views/product/FilterProduct.jsx
@@ -1,92 +1,45 @@
-import { useState } from 'react';
-import React from "react";
-import {
-  Box,
-  Typography,
-  Button,
-  Paper,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-} from "@mui/material";
-
+// ProductPage.jsx
+import React, { useState } from "react";
+import { Box, Typography, Button, Paper } from "@mui/material";
 import Footer from "../../components/ProductPage/Footer";
 import ProductGrid from "../../components/ProductPage/ProductGrid";
-
-const filtersConfig = [
-  {
-    category: 'Position',
-    options: ['Exterior', 'Interior', 'Interior and Exterior'],
-  },
-  {
-    category: 'Product type',
-    options: ['Cleaner', 'Paint', 'Undercoat', 'Varnish', 'Waterproofing'],
-  },
-  {
-    category: 'Room types',
-    options: ['Bathroom', 'Bedroom', 'Children\'s Room', 'Dining Room', 'Hallway', 'Home Office', 'Kitchen', 'Living Room'],
-  },
-  {
-    category: 'Surface',
-    options: ['Bluestone', 'Doors', 'Furniture', 'Metal', 'Walls', 'Windows', 'Wood'],
-  },
-  {
-    category: 'Finish',
-    options: ['Gloss', 'Gloss, Semi Gloss Matt', 'High Gloss', 'Low Sheen', 'Matt', 'Mid Sheen', 'NA', 'Semi Gloss'],
-  },
-];
-
-const FilterList = ({ filter, filters, handleFilterChange }) => (
-  <Box mb={3}>
-    <Typography variant="h6" gutterBottom>
-      {filter.category}
-    </Typography>
-    <FormGroup>
-      {filter.options.map((option, index) => (
-        <FormControlLabel
-          key={index}
-          control={
-            <Checkbox
-              checked={filters[`${filter.category.toLowerCase().replace(/ /g, '')}${option.replace(/ /g, '')}`]}
-              onChange={handleFilterChange}
-              name={`${filter.category.toLowerCase().replace(/ /g, '')}${option.replace(/ /g, '')}`}
-            />
-          }
-          label={option}
-        />
-      ))}
-    </FormGroup>
-  </Box>
-);
+import FilterBar, { filtersConfig } from "../../components/ProductPage/Filtering";
+import FilterList from "../../components/ProductPage/Filtering";
 
 const ProductPage = () => {
   const [filters, setFilters] = useState({
     positionExterior: false,
     positionInterior: false,
-    positionInteriorExterior: false,
-    productTypeCleaner: false,
-    productTypePaint: false,
-    productTypeUndercoat: false,
-    productTypeVarnish: false,
-    productTypeWaterproofing: false,
-    roomTypeBathroom: false,
-    roomTypeBedroom: false,
+    positionInteriorandExterior: false,
+    producttypeCleaner: false,
+    producttypePaint: false,
+    producttypeUndercoat: false,
+    producttypeVarnish: false,
+    producttypeWaterproofing: false,
+    roomtypesBathroom: false,
+    roomtypesBedroom: false,
+    roomtypesChildrensRoom: false,
+    roomtypesDiningRoom: false,
+    roomtypesHallway: false,
+    roomtypesHomeOffice: false,
+    roomtypesKitchen: false,
+    roomtypesLivingRoom: false,
+    surfaceBluestone: false,
+    surfaceDoors: false,
+    surfaceFurniture: false,
+    surfaceMetal: false,
+    surfaceWalls: false,
+    surfaceWindows: false,
+    surfaceWood: false,
+    finishGloss: false,
+    finishGlossSemiGlossMatt: false,
+    finishHighGloss: false,
+    finishLowSheen: false,
+    finishMatt: false,
+    finishMidSheen: false,
+    finishNA: false,
+    finishSemiGloss: false,
   });
-
-  const handleClearAll = () => {
-    setFilters({
-      positionExterior: false,
-      positionInterior: false,
-      positionInteriorExterior: false,
-      productTypeCleaner: false,
-      productTypePaint: false,
-      productTypeUndercoat: false,
-      productTypeVarnish: false,
-      productTypeWaterproofing: false,
-      roomTypeBathroom: false,
-      roomTypeBedroom: false,
-    });
-  };
 
   const handleFilterChange = (event) => {
     setFilters({
@@ -95,95 +48,102 @@ const ProductPage = () => {
     });
   };
 
+  const handleClearAll = () => {
+    const resetFilters = {};
+    Object.keys(filters).forEach((key) => {
+      resetFilters[key] = false;
+    });
+    setFilters(resetFilters);
+  };
+
   return (
-    <Box
-      sx={{
-        backgroundColor: '#fff',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
+    <>
       <Box
         sx={{
+          backgroundColor: '#fff',
           display: 'flex',
-          mt: 2,
-          width: '100%',
-          maxWidth: 1170,
-          gap: 2.5,
-          justifyContent: 'space-between',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
-        <Paper sx={{ width: '25%', p: 2, mr: 2 }}>
-          <Typography variant="h5" gutterBottom>
-            Filters
-          </Typography>
-          <Button variant="text" color="primary" onClick={handleClearAll}>
-            Clear All
-          </Button>
-          {filtersConfig.map((filter, index) => (
+        <Box
+          sx={{
+            display: 'flex',
+            mt: 2,
+            width: '100%',
+            maxWidth: 1170,
+            gap: 2.5,
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box sx={{ width: '25%', p: 2, mr: 2 }}>
+            <Typography variant="h5" gutterBottom>
+              Filters
+            </Typography>
+            <Button variant="text" color="primary" onClick={handleClearAll}>
+              Clear All
+            </Button>
             <FilterList
-              key={index}
-              filter={filter}
               filters={filters}
+              filtersConfig={filtersConfig}
               handleFilterChange={handleFilterChange}
             />
-          ))}
-        </Paper>
-        <Box sx={{ display: "flex", flexDirection: "column", p: 2.5, flex: 1 }}>
-          <Box sx={{ display: "flex", width: 142, maxWidth: "100%", gap: 2 }}>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-            >
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column", p: 2.5, flex: 1 }}>
+            <Box sx={{ display: "flex", width: 142, maxWidth: "100%", gap: 2 }}>
               <Box
                 sx={{
-                  borderRadius: 0.5,
-                  backgroundColor: "#ed005d",
-                  height: 40,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
                 }}
-              />
+              >
+                <Box
+                  sx={{
+                    borderRadius: 0.5,
+                    backgroundColor: "#ed005d",
+                    height: 40,
+                  }}
+                />
+              </Box>
+              <Typography
+                variant="h6"
+                sx={{ color: "#ed005d", my: "auto", fontWeight: 600 }}
+              >
+                Our Products
+              </Typography>
             </Box>
             <Typography
-              variant="h6"
-              sx={{ color: "#ed005d", my: "auto", fontWeight: 600 }}
+              variant="h4"
+              sx={{
+                color: "#000",
+                letterSpacing: 1.44,
+                mt: 2.5,
+                fontWeight: 600,
+              }}
             >
-              Our Products
+              Explore Our Products
             </Typography>
+            <ProductGrid />
+            <Button
+              variant="contained"
+              sx={{
+                borderRadius: 0.5,
+                backgroundColor: "#ed005d",
+                mt: 7.5,
+                color: "#fafafa",
+                p: "16px 48px",
+                fontWeight: 500,
+                alignSelf: 'center'
+              }}
+            >
+              View All Products
+            </Button>
           </Box>
-          <Typography
-            variant="h4"
-            sx={{
-              color: "#000",
-              letterSpacing: 1.44,
-              mt: 2.5,
-              fontWeight: 600,
-            }}
-          >
-            Explore Our Products
-          </Typography>
-          <ProductGrid />
-          <Button
-            variant="contained"
-            sx={{
-              borderRadius: 0.5,
-              backgroundColor: "#ed005d",
-              mt: 7.5,
-              color: "#fafafa",
-              p: "16px 48px",
-              fontWeight: 500,
-              alignSelf: 'center'
-            }}
-          >
-            View All Products
-          </Button>
         </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </>
   );
 };
 
