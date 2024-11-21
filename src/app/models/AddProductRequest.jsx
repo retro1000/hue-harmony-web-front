aacf18127d5baad5d6b111aea5ed3db42eceb4be
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2024-11-22 02:08:39 +0530

    model for adding product

---
+++ src/app/models/AddProductRequest.jsx
@@ -0,0 +1,27 @@
+class AddBookRequest {
+    constructor(brand, coat, coverage, dryingTime, finish, positions, productDescription, 
+        productDiscount, productName, productPrice, productStatus,productTypes,roomType,surfaces, productFeatures,productImage
+    ) {
+        this.brand = brand;
+        this.coat = coat;
+        this.coverage = coverage;
+        this.dryingTime = dryingTime;
+        this.finish = finish;
+        this.positions = positions;
+        this.productDescription = productDescription;
+        this.productDiscount = productDiscount;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productStatus = productStatus;
+        this.productTypes = productTypes;
+        this.roomType = roomType;
+        this.surfaces = surfaces;
+        this.productFeatures = productFeatures;
+        this.productImage = productImage;
+
+    }
+}
+
+export default AddBookRequest;
+
+  
