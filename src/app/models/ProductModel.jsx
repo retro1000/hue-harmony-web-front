65b13fd125056b16d630ddfb07627af251902e15
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2024-11-25 23:06:21 +0530

    Product model

---
+++ src/app/models/ProductModel.jsx
@@ -0,0 +1,31 @@
+class ProductModel {
+    constructor(brand, coat, coverage, dryingTime, finish, positions, productDescription, 
+        productDiscount, productName, productPrice, productStatus,productTypes,roomType,surfaces, productFeatures,productImage,
+        productQuantity,onlineLimit
+    ) {
+        this.id = id;
+        this.brand = brand;
+        this.coat = coat;
+        this.coverage = coverage;
+        this.dryingTime = dryingTime;
+        this.finish = finish;
+        this.productQuantity = productQuantity;
+        this.onlineLimit = onlineLimit;
+        this.positions = positions;
+        this.productDescription = productDescription;
+        this.productDiscount = productDiscount;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productStatus = productStatus;
+        this.productTypes = productTypes;
+        this.roomType = roomType;
+        this.surfaces = surfaces;
+        this.productFeatures = productFeatures;
+        this.productImage = productImage;
+
+    }
+}
+
+export default ProductModel;
+
+  
