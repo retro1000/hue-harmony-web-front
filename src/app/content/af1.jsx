506b1cc297fc933d5dcc57792610387f0ea1c9db
Akila Kushantha <akila.kushantha@gmail.com>
2024-12-01 19:59:45 +0530

    cart

--- src/app/content/af1.jsx
+++ src/app/content/af1.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const DummyComponent = () => {
+const contentComponent = () => {
   return (
     <div>
       <h1>Hello, React!</h1>
-      <p>This is a dummy component.</p>
+      <p>This is a content component.</p>
     </div>
   );
 };
 
-export default DummyComponent;
+export default contentComponent;
