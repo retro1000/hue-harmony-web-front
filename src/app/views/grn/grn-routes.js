3680c265ae19bb79e7a21eee6c88b42e689ee0c8
Akila Kushantha <akila.kushantha@gmail.com>
2024-12-04 03:19:57 +0530

    grn create connected

--- src/app/views/grn/grn-routes.js
+++ src/app/views/grn/grn-routes.js
@@ -5,26 +5,39 @@ import AuthGuard from "app/auth/AuthGuard";
 
 const GrnList = Loadable(lazy(() => import("./GrnList")));
 const GrnDetail = Loadable(lazy(() => import("./Grn")));
+const GRNCreateForm = Loadable(lazy(() => import("./Grn-create")));
 
 //grn create edit back
 //approve inventory
 const grnRoutes = [
-  { 
+  {
     path: "/grn/list",
-    element: 
-      <AuthGuard auth={[...authRoles.inventory_manager, ...authRoles.back_office]}>
-        <GrnList />
-      </AuthGuard>
+    element: (
+      // <AuthGuard auth={[...authRoles.inventory_manager, ...authRoles.back_office]}>
+      <GrnList />
+    ),
+    // </AuthGuard>
   },
   {
     path: "/grn/view/:id",
     element: (
-      <AuthGuard auth={[...authRoles.inventory_manager, ...authRoles.back_office]}>
+      <AuthGuard
+        auth={[...authRoles.inventory_manager, ...authRoles.back_office]}
+      >
         <GrnDetail />
       </AuthGuard>
     ),
   },
-  
+  {
+    path: "/grn/create",
+    element: (
+      // <AuthGuard
+      //   auth={[...authRoles.inventory_manager, ...authRoles.back_office]}
+      // >
+      <GRNCreateForm />
+      // { </AuthGuard> }
+    ),
+  },
 ];
 
 export default grnRoutes;
